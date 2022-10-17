a6f3a58686abb20505ebb264b59936494890de2b
Max <maxpakhtusov@gmail.com>
2022-10-17 19:26:28 +0300

    Toolbar is moved from activity to fragments.

--- app/src/main/java/com/sonofasleep/watertheplantapp/fragments/AddPlantFragment.kt
+++ app/src/main/java/com/sonofasleep/watertheplantapp/fragments/AddPlantFragment.kt
@@ -3,10 +3,13 @@ package com.sonofasleep.watertheplantapp.fragments
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.MenuProvider
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.sonofasleep.watertheplantapp.R
 import com.sonofasleep.watertheplantapp.databinding.FragmentAddPlantBinding
 import com.sonofasleep.watertheplantapp.databinding.FragmentPlantsListBinding
@@ -25,8 +28,21 @@ class AddPlantFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        // Replace super and add clicklistners
-        super.onViewCreated(view, savedInstanceState)
+        // Inflating toolbarMenuLayout
+        binding.plantListToolbar.inflateMenu(R.menu.menu_main)
+
+        // Hiding buttons
+        val searchButton = binding.plantListToolbar.menu.findItem(R.id.app_bar_search)
+        val sortButton = binding.plantListToolbar.menu.findItem(R.id.app_bar_sorting)
+        searchButton.isVisible = false
+        sortButton.isVisible = false
+
+        // Setting navigation
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        view.findViewById<Toolbar>(R.id.plant_list_toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/sonofasleep/watertheplantapp/fragments/PlantsListFragment.kt
+++ app/src/main/java/com/sonofasleep/watertheplantapp/fragments/PlantsListFragment.kt
@@ -3,8 +3,11 @@ package com.sonofasleep.watertheplantapp.fragments
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.sonofasleep.watertheplantapp.R
 import com.sonofasleep.watertheplantapp.databinding.FragmentPlantsListBinding
@@ -17,25 +20,28 @@ class PlantsListFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentPlantsListBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // RecyclerView
         recyclerView = binding.recyclerView
 
+        // Inflating toolbarMenuLayout
         binding.plantListToolbar.inflateMenu(R.menu.menu_main)
+
+        // Setting navigation
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        view.findViewById<Toolbar>(R.id.plant_list_toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_plantsListFragment_to_addPlantFragment)
